1df39bda2b287ad019c995d985e6d355300dbc05
vinay pancherpula <vinayvarmap1999@gmail.com>
2023-06-06 12:42:52 -0700

    hello

---
+++ demo/src/demo/one.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class one {
+	
+	public void m1() {
+		System.out.println("method 1");
+	}
+	public void m2() {
+		System.out.println("method2");
+	}
+
+	public static void main(String[] args) {
+
+		one o =new one ();
+		o.m1();
+		o.m2();
+	}
+
+}
